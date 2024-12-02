aaea1f668f5f3484ef9ac623f5e7707b9a72ee0e
Pradeep Khadka <Pradeepkhadka505@gmail.com>
2024-12-02 16:03:27 +0000

    Upaded regarding section 3

--- main.R
+++ main.R
@@ -25,22 +25,25 @@ summary(economic_data)
 nrow(economic_data)
 
 #step 3: removing NA values from each row 
-economic_data_na = na.omit(economic_data)
-economic_data_na
+economic_cleaned_data = na.omit(economic_data)
+economic_cleaned_data
 
 #Step 4: Checking all NA values in data-set after removing rows having NA values 
-summary(economic_data_na)
+summary(economic_cleaned_data)
 
 #number of rows after removing 
-nrow(economic_data_na)
+nrow(economic_cleaned_data)
 
 #step 4 : Sorting data sets using overall_score highest to lowest
-#economic_data_na <- economic_data_na %>%
-#                arrange(desc(Overall_Score))
-#economic_data_na
+economic_cleaned_data = arrange(economic_cleaned_data,desc(Overall_Score))
+economic_cleaned_data
 
-economic_data_na = arrange(economic_data_na,desc(Overall_Score))
-economic_data_na 
+#Step 5 : geting all 12 pillar data from cleaned data 
+economic_pillar_data <- economic_cleaned_data %>%
+                      filter(!Country %in% c('Cuba', 'North Korea')) %>%
+                      select(Overall_Score : Financial_Freedom)
+glimpse(economic_pillar_data)
+nrow(economic_pillar_data)
 
 #-Section 2 :
 #--------> Part-1: R to build the Scatter matrix plot using ggpairs()
@@ -51,14 +54,61 @@ library(ggplot2)
 library(GGally)
 
 #Step-2: Using ggpairs() of above library in data sets after removing NA values & sorting 
-ggpairs(economic_data_na,
-        columns = 4:12, # Adjust column indices to include numeric variables
-        aes(color = Region),
+ggpairs(economic_pillar_data,
+        aes(),
         title = "Scatter Matrix of Economic Freedom Variables by Region",
-        cardinality_threshold = 200
         )
 
+# Compute correlation matrix for the pillar variables
+correlation_matrix <- cor(economic_pillar_data, use = "complete.obs")
+# Print the correlation matrix
+print(correlation_matrix)
+
+
+library(corrplot)
+
+# Create a correlogram
+corrplot(correlation_matrix, method = "color", type = "upper", tl.cex = 0.8, addCoef.col = "black")
+
 #Step - 3 : Single-predictor Linear Model 
+model = lm(Overall_Score~Property_Rights , data = economic_pillar_data)
+model
+summary(model)
+
+#step -4 : To check Residual and fitted value and Normal Q-Q
+library(ggfortify)
+autoplot(model)
+
+
+#------ Section-3
+#---------------> Part-A:  <------------
+#==============================
+
+# Briefly explanation about the AIC (Akaike Information Criterion) when using compare the model 
+
+
+#----------------> Part-B:   <-----------
+library(olsrr)
+
+#Step-1 = linear model using only government spending and labor freedom as predictors.
+model_1 = lm(Overall_Score ~ Government_Spending + Labor_Freedom, data = economic_pillar_data)
+model_1
+summary(model_1)
+
+#Step-2 = The best two-predictor model using any of the 12 pillar variables
+model_2 <- lm(Overall_Score ~ Property_Rights + Tax_Burden, data = economic_pillar_data)
+summary(model_2)
+
+#Step-3 = The best four-predictor model using any of the 12 pillar variables
+model_3 <- lm(Overall_Score ~ Property_Rights + 
+                              Tax_Burden + 
+                              Trade_Freedom + 
+                              Fiscal_Health, 
+                          data = economic_pillar_data)
+summary(model_3)
+
+
+AIC(model_1, model_2, model_3)
 
 
 
